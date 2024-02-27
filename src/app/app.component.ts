8b3c35b895ed84b358a77c40db057a2c1e2c0972
Jefferson Assis <jbrunoassis@gmail.com>
2024-02-27 18:28:58 -0300

    Primeiro Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppModule } from './app.module';
+import { HomeComponent } from './page/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
